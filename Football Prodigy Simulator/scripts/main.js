c8a7f46671de42ceb14cc63f5378396bc45634c3
Amjed Agabani <amjedagabani@gmail.com>
2018-03-25 20:46:31 +0100

    Add files via upload

--- Football Prodigy Simulator/scripts/main.js
+++ Football Prodigy Simulator/scripts/main.js
@@ -1,3 +1,18 @@
+function UserInterface() {
+    function print(message) {
+        window.alert(message);
+    }
+
+    function input(message) {
+        return window.prompt(message);
+    }
+
+    return {
+        print: print,
+        input: input
+    }
+}
+
 function Character() {
     var stats = {
         pace: 3,
@@ -32,6 +47,28 @@ function Character() {
         }
     }
 
+    function raiseReputation() {
+        var drillPerformance = Math.random();
+        if (drillPerformance > 0.9) {
+            stats.reputation += 25;
+            return {
+                message: "As a result of a spectacular performance in training your reputation has risen by 25"
+            }
+        } else if (drillPerformance > 0.75) {
+            stats.reputation += 10;
+            return {
+                message: "As a result of an amazing performance in training your reputation has risen by 10" 
+            };
+        } else if (drillPerformance > 0.6) {
+            stats.reputation += 5;
+            return {
+                message: "As a result of a good performance in training your reputation has risen by 5"
+            };
+        }
+
+        return undefined;
+    }
+
     function dropReputation(value) {
         stats.reputation -= value;
     }
@@ -64,34 +101,35 @@ function Character() {
 
     return {
         dropReputation: dropReputation,
+        raiseReputation: raiseReputation,
         train: train,
         getStats: getStats,
         printStats: printStats
     };
 }
 
-
-var name = window.prompt("Welcome to the training camp kiddo, what is your name?");
-
+var userInterface = UserInterface();
 var character = Character();
 
-var playerPosition = window.prompt("Do you play as a forward, midfielder or defender?");
+var name = userInterface.input("Welcome to the training camp kiddo, what is your name?");
+
+var playerPosition = userInterface.input("Do you play as a forward, midfielder or defender?");
 if (playerPosition !== undefined || playerPosition !== "") {
     playerPosition = playerPosition.toLowerCase();
 }
 
 if (!name) {
-    name = window.prompt("I didn't catch your name?");
+    name = userInterface.input("I didn't catch your name?");
 }
 
 if (!name) {
-    name = window.prompt("Not the way to be starting kiddo, last chance. What's your name?");
+    name = userInterface.input("Not the way to be starting kiddo, last chance. What's your name?");
     character.dropReputation(5);
 }
 
 if (!name) {
     name = "Newbie";
-    window.alert("Alright kiddo, you're already off to a bad start. Ill just address you as " + name + ".");
+    userInterface.print("Alright kiddo, you're already off to a bad start. Ill just address you as " + name + ".");
     character.dropReputation(5);
 };
 
@@ -111,14 +149,14 @@ if (playerPosition === "defender") {
 }
 
 if (playerPosition === undefined || playerPosition === "") {
-    window.alert("Welcome " +
+    userInterface.print("Welcome " +
         name +
         ". Our job is to get you into a football club as an up and coming " +
         "player" +
         " for next season, how you perform during this training camp will affect which clubs come knocking for you.");
 
 } else {
-    window.alert("Welcome " +
+    userInterface.print("Welcome " +
         name +
         ". Our job is to get you into a football club as an up and coming " +
         playerPosition +
@@ -127,20 +165,23 @@ if (playerPosition === undefined || playerPosition === "") {
 
 while (true) {
 
-    var drillChoice = window.prompt("First thing we're doing today " +
+    var drillChoice = userInterface.input("First thing we're doing today " +
             name +
             " is training drills, what would you like to work on first? (Choose from pace, dribbling, passing, defending, heading, shooting)")
         .toLowerCase();
 
-    drillProgress = Math.floor(Math.random() * 10);
+    var drillProgress = Math.floor(Math.random() * 10);
     var result = character.train(drillChoice, drillProgress);
 
     if (result.error !== undefined) {
-        window.alert(result.error);
-        window.alert(character.printStats());
+        userInterface.print(result.error);
+        userInterface.print(character.printStats());
     } else {
-        window.alert("Your " + drillChoice + " increased by " + drillProgress + ".");
-
-        window.alert(character.printStats());
+        userInterface.print("Your " + drillChoice + " increased by " + drillProgress + ".");
+        if (drillProgress > 5) {
+            var message = character.raiseReputation();
+            userInterface.print(message.message);
+        }
+        userInterface.print(character.printStats());
     }
 }
